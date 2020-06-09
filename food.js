35b17668e0999c7c3e3f260b4b1896b16af7587f
pablorm296 <pablo.reyes.moctezuma@gmail.com>
2020-06-08 22:24:40 -0500

    Mapped fod position to grid.

--- food.js
+++ food.js
@@ -2,8 +2,12 @@ class Food {
 
     constructor() {
         // Set initial position
-        this.x = random(0 + tileSize, w - tileSize);
-        this.y = random(0 + tileSize, h - tileSize);
+        const x = random(0, w - tileSize);
+        const y = random(0, h - tileSize);
+        const x_mapped = map(x, 0, w, 0, ncols);
+        const y_mapped = map(y, 0, h, 0, nrows);
+        this.x = floor(x_mapped) * tileSize;
+        this.y = floor(y_mapped) * tileSize;
     }
 
     get coords() {
